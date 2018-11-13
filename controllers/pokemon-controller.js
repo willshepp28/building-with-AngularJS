1efe8329ddc108562476b21c39ebd36527bc0263
willshepp28 <willsheppard28@outlook.com>
2018-11-13 08:41:37 -0500

    included a image of each pokemon in pokemonCharacters array

--- controllers/pokemon-controller.js
+++ controllers/pokemon-controller.js
@@ -5,8 +5,10 @@ pokemonApp.controller("pokemonController", function($scope) {
     $scope.intro = "Welcome to the pokemon game";
 
     $scope.pokemonCharacters = [
-        { name: "Pikachu"},
-        { name: "Bulbasur"},
-        { name: "Charizard"}
-    ]
+        { name: "Pikachu", image: "https://vignette.wikia.nocookie.net/pokemon/images/0/0d/025Pikachu.png/revision/latest/scale-to-width-down/200?cb=20140328192412"},
+        { name: "Bulbasur", image: "https://vignette.wikia.nocookie.net/pokemon-planet/images/4/4e/250px-001Bulbasaur.png/revision/latest/scale-to-width-down/200?cb=20180720013708"},
+        { name: "Charizard", image: "https://vignette.wikia.nocookie.net/mario/images/7/7e/006Charizard.png/revision/latest?cb=20180903174931"}
+    ];
+
+
 });
