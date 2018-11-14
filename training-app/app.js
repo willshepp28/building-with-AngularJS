aa371e7ec5ef03263e4252da262fd02288269ec6
willshepp28 <willsheppard28@outlook.com>
2018-11-14 07:36:00 -0500

    created a array in our controller that survives minification

--- training-app/app.js
+++ training-app/app.js
@@ -1,8 +1,8 @@
-var myApp = angular.module("myApp", ["ngMessages"]); // take the name of the app and a array of dependencies
+var myApp = angular.module("myApp", ["ngMessages", "ngResource"]); // take the name of the app and a array of dependencies
 
 
 // Declare a controller
-myApp.controller("mainController", function($scope, $log, $filter){
+myApp.controller("mainController", ["$scope", "$log", function($scope, $log, $filter, $resource){
     
     $scope.name = "Will";
     $scope.formattedname = $filter("uppercase")($scope.name);
@@ -10,7 +10,7 @@ myApp.controller("mainController", function($scope, $log, $filter){
 
     $log.log($scope.name);
     $log.info($scope.formattedname)
-});
+}]);
 
 
 
