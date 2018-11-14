c35a9ba0926f22b38d952b7fb953d48e3756553a
willshepp28 <willsheppard28@outlook.com>
2018-11-14 06:46:14 -0500

    created a barebones angularjs app

---
+++ training-app/app.js
@@ -0,0 +1,7 @@
+var myApp = angular.module("myApp", []); // take the name of the app and a array of dependencies
+
+
+// Declare a controller
+myApp.controller("mainController", function(){
+    
+});
